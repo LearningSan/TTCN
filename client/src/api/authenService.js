154b4e62af89624e1df8f1601f6491aa24fdc49d
Only-San <duysannguyen113@gmail.com>
2026-01-03 21:45:23 +0700

    chinh sua client

--- client/src/api/authenService.js
+++ client/src/api/authenService.js
@@ -2,17 +2,17 @@
 import api from "./api";
 
 export const fetchMe = async () => {
-  return await api.get(`/me`);
+  return await api.get(`authen/me`);
 };
 export const updateProfileApi = async (body) => {
-  return await api.put(`/me/profile`, body);
+  return await api.put(`authen/me/profile`, body);
 };
 export const changePasswordApi =async (body) => {
-  return await api.put(`/me/password`, body);
+  return await api.put(`authen/me/password`, body);
 };
 
 export const signupApi =async (body) => {
-  return await api.post(`/signup`, body);
+  return await api.post(`authen/signup`, body);
 };
 
 export const loginApi =async (email, password) => {
@@ -23,14 +23,14 @@ export const loginApi =async (email, password) => {
 
 export const logoutApi =async () => {
   return await api.post(
-    `/logout`)
+    `authen/logout`)
 };
 // gửi OTP quên mật khẩu
 export const sendResetPasswordOtpApi =async (email) => {
-  return await api.post(`/forgot-password`, { email });
+  return await api.post(`authen/forgot-password`, { email });
 };
 
 // đặt lại mật khẩu
 export const resetPasswordApi =async ({ email, otp, newPassword }) => {
-  return await api.post(`/reset-password`, { email, otp, newPassword });
+  return await api.post(`authen/reset-password`, { email, otp, newPassword });
 };
